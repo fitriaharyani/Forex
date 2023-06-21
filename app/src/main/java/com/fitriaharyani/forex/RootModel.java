a4e97985e1a9f8783045c706db4efac32842265e
yuka3vt <fitriamelawi@gmail.com>
2023-06-21 14:21:47 +0700

    Forex

---
+++ app/src/main/java/com/fitriaharyani/forex/RootModel.java
@@ -0,0 +1,13 @@
+package com.fitriaharyani.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+    public RootModel(){}
+
+    public RatesModel getRatesModel(){return ratesModel;}
+
+    public void setRatesModel(RatesModel ratesModel){this.ratesModel = ratesModel;}
+}
